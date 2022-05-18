2a536373533c7b23b1ecf321f1ee92ccd593e252
chadd williams <will4614@pacificu.edu>
2022-05-17 21:36:40 -0700

    Update helloWorld.cpp

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
 #include <vector> 
 #include <string>
-#include "../include/goodBye.h"
-
-using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
-    goodBye();
+    std::cout << "hi";
+    return EXIT_SUCCESS;
 }
